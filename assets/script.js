2812ccb2d1c9a73a86f377c78d27f3a46900d25d
Deorren Erive <deorren@deorrens-air.attlocal.net>
2022-11-08 06:26:23 -0800

    testing

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+
+function hideItems() {
+    var x = document.getElementById('nav-items');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block'
+    }
+}
+
+
+function aboutMe() {
+    var x = document.getElementById('about-me');
+    if(x.style.display === 'block') {
+        x.style.display = 'none'
+    } else {
+        x.style.display = 'block'
+    }
+
+}
